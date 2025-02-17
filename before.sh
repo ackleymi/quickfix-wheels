96b9732b275f3512a25dd317dee7847ff664ab9f
Michael Ackley <mackley@connamara.com>
2025-02-17 15:00:48 -0600

    Add new wheel builder

---
+++ before.sh
@@ -0,0 +1,34 @@
+rm -rf quickfix
+git clone --depth 1 https://github.com/quickfix/quickfix.git
+rm -rf quickfix/.git
+
+pushd quickfix
+./configure --with-python3 --with-postgresql --with-mysql && make
+popd
+
+rm -rf quickfix-python/C++
+rm -rf quickfix-python/spec
+rm -rf quickfix-python/quickfix*.py
+rm -rf quickfix-python/doc
+rm -rf quickfix-python/LICENSE
+
+mkdir quickfix-python/C++
+mkdir quickfix-python/spec
+
+cp quickfix/LICENSE quickfix-python
+
+cp quickfix/src/python3/*.py quickfix-python
+cp quickfix/src/C++/*.h quickfix-python/C++
+cp quickfix/src/C++/*.hpp quickfix-python/C++
+cp quickfix/src/C++/*.cpp quickfix-python/C++
+cp -R quickfix/src/C++/double-conversion quickfix-python/C++
+cp quickfix/src/python3/QuickfixPython.cpp quickfix-python/C++
+cp quickfix/src/python3/QuickfixPython.h quickfix-python/C++
+
+cp quickfix/spec/FIX*.xml quickfix-python/spec
+
+touch quickfix-python/C++/config.h
+touch quickfix-python/C++/config_windows.h
+touch quickfix-python/C++/Allocator.h
+rm -f quickfix-python/C++/stdafx.*
+cp -R quickfix/src/swig "quickfix-python/C++"
